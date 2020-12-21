4f7285d3e01b2ec497150e7ded59a2d448ba5c33
Mohit Jain <choudharymohit1109@gmail.com>
2020-12-21 12:26:05 +0530

    third commit

--- src/main/webapp/assets/js/alumni-dashboard.js
+++ src/main/webapp/assets/js/alumni-dashboard.js
@@ -1,6 +1,12 @@
 let alumni_details_view = document.getElementById('view_details_button');
 let alumni_details_edit = document.getElementById('edit_details_button');
 
+let alumni_education_view = document.getElementById('view_education_button');
+let alumni_education_edit = document.getElementById('edit_education_button');
+
+let alumni_organisation_view = document.getElementById('view_organisation_button');
+let alumni_organisation_edit = document.getElementById('edit_organisation_button');
+
 alumni_details_view.addEventListener('click', async (e) => {
     console.log("Hi, alumni details view");
     location.href = "index.html";
@@ -9,6 +15,16 @@ alumni_details_view.addEventListener('click', async (e) => {
 alumni_details_edit.addEventListener('click', async (e) => {
     console.log("Hi, alumni edit");
     location.href = "alumnidetailsedit.html";
+});
+
+alumni_education_edit.addEventListener('click', async (e) => {
+    console.log("Hi, alumni_education_view");
+    location.href = "alumnieducationedit.html";
+});
+
+alumni_organisation_edit.addEventListener('click', async (e) => {
+    console.log("Hi, alumni_organisation_edit");
+    location.href = "alumniorganizationedit.html";
 });
 
 
